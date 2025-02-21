299e0ce7e7867f80da68989dd9eece09b2230c59
Bruno Unias <brunounias@gmail.com>
2025-02-21 19:50:15 -0300

    criando afunção remover

---
+++ src/assests/scripts/remover.js
@@ -0,0 +1,9 @@
+
+const remover = document.getElementById("item").addEventListener("click", function (){
+    var rew = document.getElementById("blue");
+
+    rew.remove();
+
+})
+
+console.log(remover)
